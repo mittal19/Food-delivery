3155fbcf050203edfe05e5d970968668851aca78
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2024-03-13 21:49:42 +0530

    Setted up eureka and resturant listing service

---
+++ resturantListing/resturantListing/src/main/java/com/priyanshu/resturantListing/ResturantListingApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshu.resturantListing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantListingApplication.class, args);
+	}
+
+}
---
+++ resturantListing/resturantListing/src/test/java/com/priyanshu/resturantListing/ResturantListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.resturantListing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a7d5ceab946ee121d7c00f523c8fa434ae3e8851
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2024-03-15 21:26:02 +0530

    Added save and fetchbyid restaurant service.

--- resturantListing/resturantListing/src/main/java/com/priyanshu/resturantListing/controller/RestaurantController.java
+++ resturantListing/resturantListing/src/main/java/com/priyanshu/resturantListing/controller/RestaurantController.java
@@ -5,9 +5,7 @@ import com.priyanshu.resturantListing.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,17 @@ public class RestaurantController {
     public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
         List<RestaurantDTO> restaurantDTOList = restaurantService.findAllRestaurants();
         return new ResponseEntity<>(restaurantDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/addRestaurant")
+    public ResponseEntity<RestaurantDTO> addRestaurant(@RequestBody RestaurantDTO restaurantDTO){
+        RestaurantDTO restaurantAddedDTO = restaurantService.addRestaurant(restaurantDTO);
+        return new ResponseEntity<>(restaurantAddedDTO,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchById/{id}")
+    public ResponseEntity<RestaurantDTO> fetchRestaurantById(@PathVariable Integer id){
+        ResponseEntity<RestaurantDTO> fetchedRestaurantDTO = restaurantService.findRestaurantById(id);
+        return fetchedRestaurantDTO;
     }
 }
--- resturantListing/resturantListing/src/main/java/com/priyanshu/resturantListing/service/RestaurantService.java
+++ resturantListing/resturantListing/src/main/java/com/priyanshu/resturantListing/service/RestaurantService.java
@@ -5,9 +5,12 @@ import com.priyanshu.resturantListing.entity.Restaurant;
 import com.priyanshu.resturantListing.mapper.RestaurantMapper;
 import com.priyanshu.resturantListing.repository.RestaurantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +25,17 @@ public class RestaurantService {
             RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)
         ).collect(Collectors.toList());
         return restaurantDTOList;
+    }
+
+    public RestaurantDTO addRestaurant(RestaurantDTO newRestaurantDTO){
+        Restaurant newRestaurant = RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(newRestaurantDTO);
+        newRestaurant = restaurantRepo.save(newRestaurant);
+        return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(newRestaurant);
+    }
+
+    public ResponseEntity<RestaurantDTO> findRestaurantById(Integer id){
+        Optional<Restaurant> restaurant = restaurantRepo.findById(id);
+        return restaurant.map(value -> new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+
     }
 }

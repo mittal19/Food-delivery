33b4ff84ad73f928f7aec56332f7abeb9a722ddd
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2024-03-18 23:02:55 +0530

    Food catalogue ms added

---
+++ foodcatalogue/foodcatalogue/src/main/java/com/priyanshu/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,18 @@
+package com.priyanshu.foodcatalogue.mapper;
+
+import com.priyanshu.foodcatalogue.dto.FoodItemDTO;
+import com.priyanshu.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDto(FoodItem foodItem);
+
+
+}
---
+++ foodcatalogue/foodcatalogue/src/main/java/com/priyanshu/foodcatalogue/repo/FoodItemRepo.java
@@ -0,0 +1,12 @@
+package com.priyanshu.foodcatalogue.repo;
+
+import com.priyanshu.foodcatalogue.entity.FoodItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodItemRepo extends JpaRepository<FoodItem, Integer> {
+    List<FoodItem> findByRestaurantId(Integer restaurantId);
+}
